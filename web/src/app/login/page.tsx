963ddf939806064cbd794623e00534348c18e6b2
YabTek <yeabsira.tekuamwork@a2sv.org>
2025-08-23 18:22:59 +0300

    integrate login and register to the backend

--- web/src/app/login/page.tsx
+++ web/src/app/login/page.tsx
@@ -1,35 +1,66 @@
+"use client"; 
+import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { authApi, LoginRequest } from "@/services/authApi"; 
+import { useRouter } from "next/navigation";
 
 export default function Login() {
+  const [form, setForm] = useState<LoginRequest>({
+    email: "",
+    password: ""
+  })
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setForm({...form, [e.target.name]: e.target.value});
+  }
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setLoading(true);
+    setError(null);
+
+    try {
+      const res = await authApi.login(form);
+      localStorage.setItem("token", res.data.token);
+      router.push("/chooseParticipation"); 
+    } catch (err: any) {
+      setError(err.response?.data?.message || "Login failed");
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="relative w-full h-screen flex items-center justify-center">
-      <Image
-        src="/img2.png"
-        layout="fill"
-        objectFit="cover"
-        alt="Background Image"
-      />
-
+      <Image src="/img2.png" fill className="object-cover" alt="Background Image"/>
       <div className="absolute p-8 rounded-lg shadow-2xl w-80 sm:w-98 text-center">
-      <p className="p-2 text-gray-700 font-semibold" >
-          Don't have an account? <Link href="/register" className="text-blue-700 font-bold text-md hover:underline">Signup</Link>
+        <p className="p-2 text-gray-700 font-semibold">
+          Don&apos;t have an account?{" "}
+          <Link href="/register" className="text-blue-700 font-bold text-md hover:underline">
+            Signup
+          </Link>
         </p>
-        <form className="flex flex-col text-white space-y-3">
-        <input
-          type="email"
-          placeholder="Email"
-          className="bg-black opacity-60 text-white text-lg rounded-full p-2 sm:p-4"
-          />
-        <input
-          type="password"
-          placeholder="Password"
-          className="bg-black opacity-60 text-white text-lg rounded-full p-2 sm:p-4"
-          />
-        <button className="bg-pink-500 text-white p-2 sm:p-4 rounded-full text-lg font-semibold shadow-lg hover:bg-pink-600 hover:cursor-pointer">
-          Login
-        </button>
-      </form>
+
+        <form onSubmit={handleLogin} className="flex flex-col text-gray-800 space-y-3">
+          <input type="email" name="email" placeholder="Email" value={form.email}
+            onChange={handleChange}
+            className="bg-black opacity-60 text-white text-lg rounded-full p-2 sm:p-4" required/>
+
+          <input type="password" name="password" placeholder="Password" value={form.password}
+            onChange={handleChange}
+            className="bg-black opacity-60 text-white text-lg rounded-full p-2 sm:p-4" required/>
+            
+          {error && <p className="text-red-600 text-sm">{error}</p>}
+
+          <button type="submit" disabled={loading}
+            className="bg-pink-500 text-white p-2 sm:p-4 rounded-full text-lg font-semibold shadow-lg hover:bg-pink-600 hover:cursor-pointer disabled:opacity-50">
+            {loading ? "Logging in..." : "Login"}
+          </button>
+        </form>
       </div>
     </div>
   );
--- web/src/app/register/page.tsx
+++ web/src/app/register/page.tsx
@@ -1,38 +1,82 @@
+"use client"
 import Image from "next/image";
 import Link from "next/link";
+import {useState} from "react";
+import { authApi, RegisterRequest }  from "@/services/authApi";
+import {useRouter} from "next/navigation";
 
 export default function Register() {
+  const [form, setForm] = useState<RegisterRequest>({
+    email: "",
+    password: "",
+    username: ""
+  });
+
+  const [error, setError] = useState<string | null> (null);
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setForm({...form, [e.target.name]: e.target.value});
+  }  
+
+  const handleRegister = async(e: React.FormEvent) => {
+    e.preventDefault();
+    setLoading(true);
+    setError(null);
+
+    try{
+      const res = await authApi.register(form);
+      router.push("/login");
+    }
+    catch(err: any){
+      setError(err.response?.data?.message || "Registration failed");
+    }
+    finally{
+      setLoading(false);
+    }
+
+
+  }
+
   return (
     <div className="relative w-full h-screen flex items-center justify-center">
-      <Image
-        src="/img2.png"
-        layout="fill"
-        objectFit="cover"
-        alt="Background Image"
-      />
+      <Image src="/img2.png" layout="fill" objectFit="cover" alt="Background Image"/>
 
       <div className="absolute p-8 rounded-lg shadow-2xl w-80 sm:w-98 text-center">
       <p className="p-2 text-gray-700 font-semibold" >
           Do you have an account? <Link href="/login" className="text-blue-700 font-bold text-md hover:underline">Login</Link>
         </p>
-        <form className="flex flex-col text-white space-y-4">
+        <form onSubmit={handleRegister} className="flex flex-col text-white space-y-4">
         <input
-        type="text"
-        placeholder="Name"
-        className="bg-black opacity-60 text-white text-lg rounded-full p-2 sm:p-4"
-      />
+          type="text"
+          name="username"
+          value={form.username}
+          onChange={handleChange}
+          placeholder="username"
+          className="bg-black opacity-60 text-white text-lg rounded-full p-2 sm:p-4"
+        />
         <input
           type="email"
+          name="email"
+          value={form.email}
+          onChange={handleChange}
           placeholder="Email"
           className="bg-black opacity-60 text-white text-lg rounded-full p-2 sm:p-4"
           />
         <input
           type="password"
+          name="password"
+          value={form.password}
+          onChange={handleChange}
           placeholder="Password"
           className="bg-black opacity-60 text-white text-lg rounded-full p-2 sm:p-4"
           />
-        <button className="bg-pink-500 text-white p-2 sm:p-4 rounded-full text-lg font-semibold shadow-lg hover:bg-pink-600 hover:cursor-pointer">
-          SIGNUP
+        {error && <p className="text-red-600 text-sm">{error}</p>}
+
+        <button className="bg-pink-500 text-white p-2 sm:p-4 rounded-full text-lg font-semibold shadow-lg hover:bg-pink-600 hover:cursor-pointer"
+        disabled={loading}>
+          {loading ? "Signing up..." : "SIGNUP"}
         </button>
       </form>
         
