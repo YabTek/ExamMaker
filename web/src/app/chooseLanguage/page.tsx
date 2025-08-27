82e4299e98813f9dea791a89180d0d3f14dcf719
YabTek <yeabsira.tekuamwork@a2sv.org>
2025-08-27 15:51:52 +0300

    update ui

--- web/src/app/chooseLanguage/page.tsx
+++ web/src/app/chooseLanguage/page.tsx
@@ -1,14 +1,37 @@
+"use client"
 import Image from "next/image";
 import { Frijole } from "next/font/google";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const frijole = Frijole({ weight: "400", subsets: ["latin"] });
 
 export default function ChooseLanguage() {
+    const router = useRouter();
+    const [selected, setSelected]  = useState<string | null>(null);
+    const [error, setError]  = useState<string | null>(null);
+
+    const participationType = localStorage.getItem("participationType");
+
+    const handleClick = () => {
+        if (selected){
+            localStorage.setItem("language", selected);
+        }
+        else{
+            setError("Please select a language")
+        }
+    };
+    const getBtnClass = (lang: string) => 
+        `${frijole.className} text-2xl font-bold rounded-2xl shadow-2xl p-4 sm:px-10 text-cyan-300 bg-blue-700 opacity-90 cursor-pointer 
+     ${selected === lang ? "border-3 border-cyan-200" : "border-none"}`;
+
+    
+
   return (
     <div className="relative w-full h-screen flex items-center justify-center">
       <Image
-        src="/img3.png"
+        src="/img12.png"
         layout="fill"
         objectFit="cover"
         alt="Background Image"
@@ -16,40 +39,47 @@ export default function ChooseLanguage() {
 
       <div className="absolute flex flex-col text-center pt-32">
         <h1
-          className={`${frijole.className} text-2xl sm:text-4xl lg:text-5xl font-bold text-blue-700 mt-[-6rem] mb-10`}>
+          className={`${frijole.className} text-2xl sm:text-4xl lg:text-5xl font-bold text-blue-700 mt-[-6rem] mb-8`}>
           PICK A LANGUAGE <br /> FOR YOUR QUIZ
         </h1>
 
         <div className="flex space-x-6 justify-center mb-6">
-            <button
-              className={`${frijole.className} bg-blue-700 text-cyan-300 text-2xl font-bold rounded-2xl shadow-2xl p-4 sm:px-10 hover:bg-blue-600`}>
+            <button onClick={() => setSelected("Python")} className={getBtnClass("Python")}>
               Python
             </button>
-            <button
-              className={`${frijole.className} bg-blue-700 text-cyan-300 text-2xl font-bold rounded-2xl shadow-2xl p-4 sm:px-10 hover:bg-blue-600`}>
+            <button onClick={() => setSelected("Java")} className={getBtnClass("Java")}>
               Java
             </button>
         </div>
 
         <div className="flex space-x-6 justify-center mb-10">
-            <button
-              className={`${frijole.className} bg-blue-700 text-cyan-300 text-2xl font-bold rounded-2xl shadow-2xl p-4 sm:px-10 hover:bg-blue-600`}>
+            <button onClick={() => setSelected("JavaScript")} className={getBtnClass("JavaScript")}>
               JavaScript
             </button>
-            <button
-              className={`${frijole.className} bg-blue-700 text-cyan-300 text-2xl font-bold rounded-2xl shadow-2xl p-4 sm:px-10 hover:bg-blue-600`}>
+            <button onClick={() => setSelected("TypeScript")} className={getBtnClass("TypeScript")}>
               TypeScript
             </button>
         </div>
 
         <div className="flex justify-center">
-          <Link href="generateQuestions">
-            <button
-              className={`${frijole.className} bg-pink-500 text-white text-2xl font-bold rounded-2xl shadow-2xl px-12 py-4 hover:bg-pink-600 hover:cursor-pointer`}>
-              Start Quiz
-            </button>
+          {participationType === "solo" ? (
+            <Link href="generateQuestions">
+              <button onClick={handleClick}
+                className={`${frijole.className} bg-pink-500 text-white text-2xl font-bold rounded-2xl shadow-2xl px-12 py-4 hover:bg-pink-600 hover:cursor-pointer opacity-90`}>
+                Start Quiz
+              </button>
           </Link>
+          ) : (
+             <Link href="addPeople">
+              <button onClick={handleClick}
+                className={`${frijole.className} bg-pink-500 text-white text-2xl font-bold rounded-2xl shadow-2xl px-12 py-4 hover:bg-pink-600 hover:cursor-pointer opacity-90`}>
+                Next
+              </button>
+          </Link>
+          )} 
         </div>
+        {error && <p className="text-red-600 text-sm">{error}</p>}
+
       </div>
     </div>
   );
--- web/src/app/chooseParticipation/page.tsx
+++ web/src/app/chooseParticipation/page.tsx
@@ -1,28 +1,38 @@
+"use client"
 import Image from "next/image";
 import { Frijole } from "next/font/google";
+import { useRouter } from "next/navigation";
 
 const frijole = Frijole({ weight: "400", subsets: ["latin"] });
 
-
 export default function Participate() {
+  const router = useRouter();
+
+  const handleSelect = (type: string) => {
+    localStorage.setItem("participationType", type);
+    router.push("/chooseLanguage");
+  };
+
+
   return (
     <div className="relative w-full h-screen flex items-center justify-center">
-      <Image
-        src="/img3.png"
-        layout="fill"
-        objectFit="cover"
-        alt="Background Image"
-      />
+      <Image src="/img11.png" layout="fill" objectFit="cover" alt="Background" />
 
       <div className="absolute flex flex-col text-center">
-      <h1 className={`${frijole.className} text-2xl sm:text-4xl lg:text-5xl font-bold text-blue-700 mt-[-6rem] mb-6`}>
-      HOW DO YOU WANT TO<br /> PARTICIPATE IN THIS QUIZ?
+        <h1 className={`${frijole.className} text-2xl sm:text-4xl lg:text-5xl font-bold text-blue-700 mt-[-6rem] mb-6`}>
+          HOW DO YOU WANT TO<br /> PARTICIPATE IN THIS QUIZ?
         </h1>
+
         <div className="space-x-4 justify-center">
-          <button className={`${frijole.className} bg-blue-700  text-cyan-300 text-2xl font-bold rounded-2xl shadow-2xl p-2 sm:py-4 sm:px-26 hover:bg-blue-600 hover:cursor-pointer`}>
+          <button
+            onClick={() => handleSelect("solo")}
+            className={`${frijole.className} bg-blue-700 text-cyan-300 text-2xl font-bold rounded-2xl shadow-2xl p-2 sm:py-4 sm:px-26 hover:bg-blue-600 hover:cursor-pointer`}>
             Alone
           </button>
-          <button className={`${frijole.className} bg-blue-700  text-cyan-300 text-2xl font-bold rounded-2xl shadow-2xl p-2 sm:py-4 sm:px-26 hover:bg-blue-600 hover:cursor-pointer`}>
+
+          <button
+            onClick={() => handleSelect("group")}
+            className={`${frijole.className} bg-blue-700 text-cyan-300 text-2xl font-bold rounded-2xl shadow-2xl p-2 sm:py-4 sm:px-26 hover:bg-blue-600 hover:cursor-pointer`}>
             Group
           </button>
         </div>
