2587f034135f2d43b97ecb529a424c088fdcdf9a
YabTek <yeabsira.tekuamwork@a2sv.org>
2025-10-07 20:33:23 +0200

    code change

--- api/src/repositories/quizRepository.ts
+++ api/src/repositories/quizRepository.ts
@@ -1,20 +1,46 @@
 import { QuizModel, Quiz } from "../models/quizModel";
 
-async function createQuiz(mode: string, questions: Quiz["questions"]) {
-  return QuizModel.create({ mode, questions });
+async function createQuiz(mode: string, questions: Quiz["questions"], player) {
+  return QuizModel.create({ mode, questions, players: [
+      {
+        id: player.id,
+        username: player.username,
+        isHost: player.isHost,
+      },
+    ], });
 };
 
 async function getQuizById(id: string){
-  return QuizModel.findById(id);
+  return QuizModel.findById(id).lean();
 };
 
 async function getAllQuizzes(){
   return QuizModel.find();
 };
 
+async function addPlayer(quizId, player) {
+  return await QuizModel.findByIdAndUpdate(
+    quizId,
+    { $push: { players: player } },
+    { new: true }
+  );
+}
+
+export async function updatePlayerScore(quizId: string, playerId: string, score: number) {
+  const updatedQuiz = await QuizModel.findOneAndUpdate(
+    { _id: quizId, "players._id": playerId },
+    { $set: { "players.$.score": score } },
+    { new: true } 
+  );
+
+  return updatedQuiz;
+}
+
 
 export default {
   createQuiz,
   getQuizById,
   getAllQuizzes,  
+  addPlayer,
+  updatePlayerScore
 }
