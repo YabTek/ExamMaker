be9f68ab15766ef652cc3c7d6b5e8639a314086a
YabTek <yeabsira.tekuamwork@a2sv.org>
2025-10-29 20:17:08 +0100

    add socket

--- api/src/server.ts
+++ api/src/server.ts
@@ -3,13 +3,38 @@ import cors from "cors";
 import dotenv from "dotenv";
 import { connectDB } from "./db";
 import * as userRoutes from "./routes/index"
+import * as quizRoutes from "./routes/index"
 import { AppError } from "./core/errors";
-
+import http from "http";
+import { Server } from "socket.io";
 
 dotenv.config();
 const app = express();
 
-app.use(cors());
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on("connection", (socket) => { 
+  socket.on("player_joined", (updatedPlayers) => {
+    socket.broadcast.emit("player_joined", updatedPlayers);
+  });
+  socket.on("start_quiz", () =>{
+    io.emit("quiz_started");
+  });
+  socket.on("disconnect", () => {
+    console.log("disconnected", socket.id);
+  });
+})
+
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+}));
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -17,6 +42,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api", userRoutes.authRouter)
+app.use('/api', quizRoutes.quizRouter);
 
 const PORT = process.env.PORT || 5000;
 
@@ -30,7 +56,7 @@ app.use((err, req, res, next) => {
 });
 
 
-app.listen(PORT, async () => {
+server.listen(PORT, async () => {
   await connectDB();
   console.log(`Server running on port ${PORT}`);
 });
