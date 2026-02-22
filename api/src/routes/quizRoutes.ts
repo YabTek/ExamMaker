660b8bf230be6dd3af6c310ba0a7bda503f8fe2b
YabTek <yeabsira.tekuamwork@a2sv.org>
2026-02-22 19:23:46 +0100

    add routes

--- api/src/routes/quizRoutes.ts
+++ api/src/routes/quizRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createQuiz, joinQuiz, fetchQuiz, updateScore } from "../controllers/quizControllers";
+import { createQuiz, joinQuiz, fetchQuiz, updateScore, getTimeLeft, startQuiz } from "../controllers/quizControllers";
 import { authMiddleware } from "../middlewares/authMiddleware";
 
 const quizRouter = Router();
@@ -7,6 +7,8 @@ const quizRouter = Router();
 quizRouter.post("/createQuiz", authMiddleware, createQuiz);
 quizRouter.post("/joinQuiz/:id", authMiddleware, joinQuiz);
 quizRouter.get("/fetchQuiz/:id", fetchQuiz);
+quizRouter.post("/startQuiz/:id", startQuiz);
+quizRouter.get("/timeLeft/:id", getTimeLeft);
 quizRouter.post("/updateScore/:id", authMiddleware, updateScore);
 
 export default quizRouter;
